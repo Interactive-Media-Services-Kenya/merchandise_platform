d9827a86ad56c26c4fe46c6400d46eed473fc09f
SteveOwago <steveowago@gmail.com>
2022-03-08 08:30:38 +0300

    County Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
         $this->call([
+                CountiesTableSeeder::class,
                 RolesTableSeeder::class,
                 UsersTableSeeder::class,
             ]);
