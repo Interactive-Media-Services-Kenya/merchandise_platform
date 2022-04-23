f3a51f70504dca7a378b6d05ffe425610ae23a38
SteveOwago <steveowago@gmail.com>
2022-04-23 08:42:49 +0300

    Increase Password Validation

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,6 +50,7 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
+        // ? Increase Password Complexity and validation rules
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
