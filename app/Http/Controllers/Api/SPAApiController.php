7fa969b28307ac4a6a2dd94d621b6179f06011a1
SteveOwago <steveowago@gmail.com>
2022-05-11 13:18:02 +0300

    ++

--- app/Http/Controllers/Api/SPAApiController.php
+++ app/Http/Controllers/Api/SPAApiController.php
@@ -79,10 +79,8 @@ class SPAApiController extends Controller
             $product_id = Product::where('product_code', $request->product_code)->value('id');
 
             $product = Product::where('id',$product_id)->first();
-
-
-
-            if (count($product)>0) {
+            dd($product);
+            if ($product) {
                 $product->update([
                     'accept_status' => 1,
                 ]);
