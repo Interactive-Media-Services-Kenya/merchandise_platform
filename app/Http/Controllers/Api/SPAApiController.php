faf121d3282ab6422b815e048e4f453dd3fd529b
SteveOwago <steveowago@gmail.com>
2022-07-28 11:48:07 +0300

    Fix upload single and multiple merchandise--request all

--- app/Http/Controllers/Api/SPAApiController.php
+++ app/Http/Controllers/Api/SPAApiController.php
@@ -797,7 +797,7 @@ class SPAApiController extends Controller
             $size = $pr['size'];
             $color = $pr['color'];
 
-
+            logger($product_code);
             $alreadyUploadedCode = DB::table('products')->whereproduct_code($product_code)->value('product_code');
             $validCode = DB::table('product_codes')->whereproduct_code($product_code)->where('product_code', '!=', $alreadyUploadedCode)->value('product_code');
 
