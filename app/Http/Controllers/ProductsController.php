ead68bcc59f353bd5913ebddfdf73376d1099ad8
SteveOwago <steveowago@gmail.com>
2022-10-21 11:06:43 +0300

    Working on distance

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -1920,9 +1920,9 @@ class ProductsController extends Controller
 
         $distanceDifference = $this->getLocationDistance->vincentyGreatCircleDistance(
             $outlet->address_latitude, $outlet->address_longitude, $request->latitude, $request->longitude, $earthRadius = 6371000);
-        dd($distanceDifference);
+        //dd($distanceDifference);
         if($distanceDifference > 1){ //If location distance is greater than 1 km
-            Alert::error('Failed', 'Issue Merchandise Out of Outlet Area');
+            Alert::error('Failed', 'Issue Merchandise Out of Outlet Area. Distance From Outlet: '.$distanceDifference.'kms');
             return back();
         }
         //Check if customer phone exist in DB and Reject
