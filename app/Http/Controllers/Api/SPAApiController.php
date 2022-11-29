536d175a176389e48a157e16dc8f06600c98a599
SteveOwago <steveowago@gmail.com>
2022-11-29 14:09:25 +0300

    Batch Agency Confirmation Status

--- app/Http/Controllers/Api/SPAApiController.php
+++ app/Http/Controllers/Api/SPAApiController.php
@@ -377,8 +377,8 @@ class SPAApiController extends Controller
                 foreach ($batch as $bch) {
                     $item = [
                         'batch' => $bch->batch_code,
-                        'confirm_status' => $pb->accept_status != 1 ? false : true,
-                        'reject_status' => $pb->reject_status == 1 ? true : false,
+                        'confirm_status' => $bch->accept_status != 1 ? false : true,
+                        'reject_status' => $bch->reject_status == 1 ? true : false,
                         'product_count' => DB::table('products')->wherebatch_id($pb->id)->count(),
                         'merchandise_type' => DB::table('categories')->whereid(Product::wherebatch_id($pb->id)->value('category_id'))->value('title'),
                     ];
