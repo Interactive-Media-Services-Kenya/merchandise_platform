c1f70ceba5adb894105f53a78f0d1c97a5920aa4
SteveOwago <steveowago@gmail.com>
2023-06-23 11:24:42 +0300

    Fix Client foreign id bug

--- database/seeders/ClientsTableSeeder.php
+++ database/seeders/ClientsTableSeeder.php
@@ -16,6 +16,7 @@ class ClientsTableSeeder extends Seeder
     {
         $clients = [
             [
+                'id' => 1,
                 'name' => 'East African Breweries Limited',
                 'email' => 'info@eabl.com',
                 'phone' => mt_rand(254700000000, 254799999999),
@@ -23,6 +24,7 @@ class ClientsTableSeeder extends Seeder
                 'address' => 'P.O Box 1234 Nairobi',
             ],
             [
+                'id' => 2,
                 'name' => 'Coca Cola Kenya',
                 'email' => 'info@cokekenya.com',
                 'phone' => mt_rand(254700000000, 254799999999),
@@ -30,6 +32,7 @@ class ClientsTableSeeder extends Seeder
                 'address' => 'P.O Box 1234 Nairobi',
             ],
             [
+                'id' => 3,
                 'name' => 'Brookside Diaries',
                 'email' => 'info@brookside.com',
                 'phone' => mt_rand(254700000000, 254799999999),
@@ -37,6 +40,7 @@ class ClientsTableSeeder extends Seeder
                 'address' => 'P.O Box 1234 Nairobi',
             ],
             [
+                'id' => 4,
                 'name' => 'Telcom Kenya',
                 'email' => 'info@telcom.com',
                 'phone' => mt_rand(254700000000, 254799999999),
